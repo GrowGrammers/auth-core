da8f31b66fbbaec1f72f0ea2519d8bd9f6d321ab
Dhani5703 <dems9811@naver.com>
2025-08-04 16:47:01 +0900

    feat: 진입점 export 구조 정리 및 주요 클래스, 인터페이스, 타입 추가 - 진입점에서 주요 클래스, 인터페이스, 타입 및 팩토리 함수들을 명확히 export하여 코드 구조를 개선함 - 저장소 구현체들을 필요시에만 export하도록 설정

--- index.ts
+++ index.ts
@@ -1,1 +1,32 @@
-// 진입점 export 정리 
+// 진입점 export 정리
+
+// 주요 클래스들
+export { AuthManager } from './AuthManager';
+
+// 인터페이스들
+export { 
+  AuthProvider, 
+  AuthProviderConfig,
+  EmailVerificationRequest,
+  EmailVerificationResponse,
+  BaseAuthProvider,
+  EmailAuthProvider,
+  GoogleAuthProvider
+} from './providers';
+export { TokenStore } from './storage/TokenStore.interface';
+
+// 타입들
+export { Token, UserInfo, AuthProviderType } from './types';
+
+// 팩토리 함수들
+export { createAuthProvider } from './factories/AuthProviderFactory';
+export { createTokenStore, TokenStoreType } from './factories/TokenStoreFactory';
+export { 
+  createAuthManager, 
+  createAuthManagerFromInstances
+} from './factories/AuthManagerFactory';
+
+// 저장소 구현체들 (필요시에만 export)
+export { WebTokenStore } from './storage/WebTokenStore';
+export { MobileTokenStore } from './storage/MobileTokenStore';
+export { FakeTokenStore } from './storage/FakeTokenStore'; 
