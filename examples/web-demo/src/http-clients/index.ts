cc1ee744c0d572e04e0537c497d31df3a0a1f4b5
Dhani5703 <dems9811@naver.com>
2025-09-09 12:40:01 +0900

    refactor: 웹 데모 개별 프로젝트 폴더로 수정

--- examples/web-demo/src/http-clients/index.ts
+++
@@ -1,4 +0,0 @@
-// HttpClient 구현체들을 한 곳에서 export
-export { RealHttpClient } from './RealHttpClient';
-export { MockHttpClient } from './MockHttpClient';
-export { MSWHttpClient } from './MSWHttpClient';
